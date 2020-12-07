b148975e6276633d9df29ef4628d1fb21ad29830
vgautam <vasundhara1997@gmail.com>
2020-12-06 22:13:37 -0800

    cleaned up a bit, but there's got to be a better way of doing this eg discarding rules you've already seen before

--- day7.py
+++ day7.py
@@ -3,54 +3,21 @@
 import re
 
 rules = {}
-with open('2020day7input', 'r', encoding='utf-8') as fo:
-    for line in fo:
-        _spl = line.split(' contain ')
-        rules[_spl[0].split(' bags')[0]] = _spl[1].strip()
+for line in open('2020day7input'):
+    v = [m.group(2) for m in re.finditer('(\d+) (\w+ \w+)', line) for n in range(int(m.group(1)))]
+    rules[re.match('^\w+ \w+', line).group()] = v
 
-def get_bags_holding(bag):
-    bags = set()
-    for k,v in rules.items():
-        if bag in v:
-            bags.add(k)
-    return bags
+def get_bags_containing(bag):
+    return {k for k in rules if bag in rules[k]}
 
-def recurse(bag):
-    holding_bags = set()
-    holding = get_bags_holding(bag)
-    print(bag, ': ', holding)
-    holding_bags.update(holding)
-    print(holding_bags)
-    if len(holding) > 0:
-        for bag in holding:
-            holding_bags.update(recurse(bag))
-    return holding_bags
-
-print(get_bags_holding('shiny gold'))
-print(recurse('shiny gold'))
-print(len(recurse('shiny gold')))
-print('part 1')
-
-
-def get_bags_holding(bag):
+def recurse(bag, func):
     bags = []
-    for raw in rules[bag].split(','):
-        for b in re.finditer('(\d+) (\w+ \w+)', raw):
-            bags += [b.group(2)]*int(b.group(1))
+    current_bags = list(func(bag))
+    bags += current_bags
+    if len(current_bags) > 0:
+        for b in current_bags:
+            bags += recurse(b, func)
     return bags
 
-def recurse(bag):
-    holding_bags = []
-    holding = get_bags_holding(bag)
-    print(bag, ': ', holding)
-    holding_bags += holding
-    print(holding_bags)
-    if len(holding) > 0:
-        for bag in holding:
-            holding_bags += recurse(bag)
-    return holding_bags
-
-print(get_bags_holding('shiny gold'))
-print(recurse('shiny gold'))
-print(len(recurse('shiny gold')))
-print('part 2')
+print(f"part 1: {len(set(recurse('shiny gold', get_bags_containing)))}")
+print(f"part 2: {len(recurse('shiny gold', rules.get))}")
