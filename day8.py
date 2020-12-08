71204eed4698544a0b28bba686edbacf15135b8f
vgautam <vasundhara1997@gmail.com>
2020-12-07 22:43:04 -0800

    cleaned up day 8

--- day8.py
+++ day8.py
@@ -1,56 +1,34 @@
 #!/usr/bin/env python3
 
-instructions = []
-with open('2020day8input', 'r', encoding='utf-8') as fo:
-    for line in fo:
-        instructions.append(line.split())
+instructions = [l for l in open('2020day8input').readlines()]
 
-#print(instructions)
-def computer(instructions):
-    acc = 0
+def computer(instructions, swap=-1):
+    accumulator = 0
     i = 0
     seen = set()
     while i < len(instructions):
-        #print(i, len(instructions))
         if i in seen:
-            #print('bad', acc)
-            return 1, acc
+            return 1, accumulator
         seen.add(i)
-        #print(i)
-        if instructions[i][0] == 'acc':
-            acc += int(instructions[i][1])
+        op, arg = instructions[i].split()
+        if swap == i and op != 'acc':
+            op = 'jmp' if op == 'nop' else 'nop'
+        if op == 'acc':
+            accumulator += int(arg)
             i += 1
-            continue
-        if instructions[i][0] == 'nop':
-            i+= 1
-            continue
-        if instructions[i][0] == 'jmp':
-            i+= int(instructions[i][1])
-    return 0, acc
+        elif op == 'nop':
+            i += 1
+        elif op == 'jmp':
+            i += int(arg)
+        else:
+            print("this shouldn't happen")
+    return 0, accumulator
 
-def swap(instruction):
-    if instruction[0] == 'jmp':
-        return ['nop', instruction[1]]
-    elif instruction[0] == 'nop':
-        return ['jmp', instruction[1]]
-    return instruction
+print(f'part 1: {computer(instructions)[1]}')
 
-print('part 1')
-print(computer(instructions))
-
-instructions = []
-with open('2020day8input', 'r', encoding='utf-8') as fo:
-#with open('test', 'r', encoding='utf-8') as fo:
-    for line in fo:
-        instructions.append(line.split())
-
-print('part 2')
-for j in range(len(instructions)):
-    #print(j)
-    #print(instructions)
-    new_instructions = [i for i in instructions]
-    new_instructions[j] = swap(new_instructions[j])
-    exitstatus, result = computer(new_instructions)
+jmps_and_nops = [i for i,instruction in enumerate(instructions) if 'acc' not in instruction]
+for i in jmps_and_nops:
+    exitstatus, result = computer(instructions, swap=i)
     if not exitstatus:
-        print(result)
-    #print(exitstatus, result)
+        print(f'part 2: {result}')
+        break
