39a3e401de81a8524cde7199a0bc6ba26fd1d3cd
vgautam <vasundhara1997@gmail.com>
2020-12-12 05:20:54 -0800

    committing v3 of my template for safekeeping

---
+++ template.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+day=3 # update me
+inp_f = f'2020day{day:02d}input'
+#inp_f = 'test'
+
+#lines = open(inp_f).read().split() # every word
+#lines = open(inp_f).readlines() # every line + \n
+#lines = [l.strip() for l in open(inp_f)] # every line
+#lines = [l.strip() for l in open(inp_f).read().split('\n\n')] # every block separated by 2 newlines
+#lines = [l.split() for l in open(inp_f).read().split('\n\n')] # every block separated by 2 newlines, split into words
+
+print(lines)
+
+print('part 1')
+
+print('part 2')
