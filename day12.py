b3b81d24f2fd0e86fa283e754971491d0f7e5096
vgautam <vasundhara1997@gmail.com>
2020-12-11 22:32:22 -0800

    day 12 part 2

--- day12.py
+++ day12.py
@@ -46,3 +46,42 @@ for l in lines:
 print(abs(coords[0])+abs(coords[1]))
 
 print('part 2')
+
+def turn(coords, direction, magnitude):
+    assert magnitude in [90, 180, 270], magnitude
+    if magnitude == 180:
+        return (-coords[0], -coords[1])
+    elif magnitude == 90:
+        if direction == 'R':
+            return (-coords[1], coords[0])
+        return (coords[1], -coords[0])
+    return turn(turn(coords, direction, 180), direction, 90)
+
+# print(turn((10,5), 'L', 180))
+# print(turn((10,5), 'R', 180))
+# print(turn((10,5), 'L', 90))
+# print(turn((10,5), 'R', 90))
+# print(turn((10,5), 'L', 270))
+# print(turn((10,5), 'R', 270))
+
+def move(coords, direction, magnitude):
+    mapping = {'E': (coords[0], coords[1]+magnitude),
+               'W': (coords[0], coords[1]-magnitude),
+               'N': (coords[0]+magnitude, coords[1]),
+               'S': (coords[0]-magnitude, coords[1])}
+    if direction in ['L', 'R']:
+        return turn(coords, direction, magnitude)
+    return mapping[direction]
+
+coords = (0,0)
+wp_coords = (1,10)
+for l in lines:
+    #print(coords, wp_coords, l)
+    direction = l[0]
+    magnitude = int(l[1:])
+    if direction != 'F':
+        wp_coords = move(wp_coords, direction, magnitude)
+    else:
+        coords = (coords[0] + magnitude*wp_coords[0], coords[1] + magnitude*wp_coords[1])
+
+print(abs(coords[0])+abs(coords[1]))
