43dd80efb1b17c45d791dda61f9db1520a491462
vgautam <vasundhara1997@gmail.com>
2020-12-12 22:40:04 -0800

    day 13 notes for later

--- day13.py
+++ day13.py
@@ -15,6 +15,23 @@ for bus in buses:
 bus = min(departure, key=departure.get)
 print('part1:', bus*(departure[bus]-earliest_time))
 
+# Some notes for later:
+# If I have [7,13] in my list of buses then to solve part 2 I need to find timestamp t (a natural number) such that
+# 7 * x = t (for some natural number x), AND
+# 13 * y = t + 1 (for some natural number y)
+# So there's many options for x and y that would work.
+# I feel like the natural number bit has to be relevant somehow.
+# This is saying the same thing as
+# t mod 7 == 0
+# t mod 13 == 1
+# but idk how to solve equations with mods.
+# One thing I could potentially do is to take my current approach and parallelize it over pairs of
+# numbers, e.g., if I have a list that looks like [7,x,x,x,13,x,x,x,29], then I could brute force
+# the solution to [7,x,x,x,13] and [13,x,x,x,29] and then... do something with that?
+# Like take the max timestamp of the partial results and re-check all of them and go up from there?
+# Also why are all the numbers in my input prime. This seems highly suspicious and I'm even more
+# convinced that this is some clever math thing I've never heard of
+
 buses = [bus for bus in open(inp_f).readlines()[1].strip().split(',')]
 
 # solve for t
