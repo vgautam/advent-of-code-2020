49ddd287ef01ebe6affe78ec991736a4654a65f5
vgautam <vasundhara1997@gmail.com>
2020-12-16 23:18:23 -0800

    day 17 pt 2
    someday i'll come back and make everything faster

--- day17.py
+++ day17.py
@@ -7,14 +7,29 @@ inp_f = f'2020day{day:02d}input'
 
 lines = [[c for c in l] for l in open(inp_f).read().split()]
 
-pocket = defaultdict(lambda: '.')
+print('part 1')
 
-def nearby_coords(coords):
+def initialize_pocket(dims=3):
+    pocket = defaultdict(lambda: '.')
+    for x,row in enumerate(lines):
+        for y,col in enumerate(row):
+            coords = (x,y,0)
+            if dims == 4:
+                coords = (x,y,0,0) 
+            pocket[coords] = col
+    return pocket
+
+def nearby_coords(coords, dims=3):
     for x in [-1,0,1]:
         for y in [-1,0,1]:
             for z in [-1,0,1]:
-                if not (x == 0 and y == 0 and z == 0):
+                if dims == 3 and not (x == 0 and y == 0 and z == 0):
                     yield(coords[0]+x, coords[1]+y, coords[2]+z)
+                if dims == 4:
+                    for aa in [-1,0,1]: # ha, excel reference
+                        if not (x == 0 and y == 0 and z == 0 and aa == 0):
+                            yield(coords[0]+x, coords[1]+y, coords[2]+z, coords[3]+aa)
+                
 
 #print(len(list(nearby_coords((1,2,3)))))
 
@@ -28,23 +43,18 @@ def updated_value(state, neighbours):
         return '#'
     return '.'
 
-print('part 1')
-for x,row in enumerate(lines):
-    for y,col in enumerate(row):
-        coords = (x,y,0)
-        pocket[coords] = col
 
-def simulate(pocket):
+def simulate(pocket, dims=3):
     new_pocket = defaultdict(lambda: '.')
     for coords in list(pocket.keys()):
         orig_value = pocket[coords]
-        neighbours = [pocket[n] for n in nearby_coords(coords)]
-        assert len(neighbours) == 26, len(neighbours)
+        neighbours = [pocket[n] for n in nearby_coords(coords, dims=dims)]
         new_pocket[coords] = updated_value(orig_value, neighbours)
     return new_pocket
-        
+
+pocket = initialize_pocket()
 for i in range(6):
-    for coords in [k for k in pocket]: # if pocket[k] == '#']:
+    for coords in [k for k in pocket]:
         for n in nearby_coords(coords):
             if n not in pocket:
                 pocket[n] = '.'
@@ -53,3 +63,13 @@ for i in range(6):
 print(list(pocket.values()).count('#'))
 
 print('part 2')
+
+pocket = initialize_pocket(dims=4)
+for i in range(6):
+    for coords in [k for k in pocket]:
+        for n in nearby_coords(coords, dims=4):
+            if n not in pocket:
+                pocket[n] = '.'
+    pocket = simulate(pocket, dims=4)
+
+print(list(pocket.values()).count('#'))
