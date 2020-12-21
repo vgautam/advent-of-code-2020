09b9ebc57730ed6608f7bf1b515eae9955c94f5a
vgautam <vasundhara1997@gmail.com>
2020-12-21 02:30:33 -0800

    day 21 pt 2 also very messy

--- day21.py
+++ day21.py
@@ -23,8 +23,6 @@ for l in lines:
             allergen_word_dict[allergen].update(words)
             allergen_count[allergen] += 1
 
-print(allergen_word_dict)
-print(all_words)
 final_allergen_word_map = {}
 words_accounted_for = set()
 sus_words = set()
@@ -35,7 +33,7 @@ for allergen in allergen_word_dict:
     max_count = max([v for k,v in counter.items()])
     max_words = []
     for word in counter:
-        if word in all_words and counter[word] == max_count:
+        if word not in words_accounted_for and counter[word] == max_count:
             if max_words:
                 unanimous_max = False
             max_words.append(word)
@@ -50,16 +48,37 @@ for allergen in allergen_word_dict:
     else:
         print('problem zone')
         print(allergen, allergen_count[allergen], max_count, max_words, unanimous_max)
-print(final_allergen_word_map)
-print(sus_words)
 
-print('finally')
+print('part 1')
 ok_words = all_words - words_accounted_for - sus_words
 total = 0
 for ingredients in recipes:
     total += sum([1 for word in ingredients if word in ok_words])
 print(total)
 
-print('part 1')
-
 print('part 2')
+words_accounted_for.update(ok_words)
+
+while len(sus_words):
+    # lots of duplication from part 1 but i cbf
+    sus_words = set()
+    for allergen in allergen_word_dict: #sus_words - set(final_allergen_word_map.values()):
+        counter = allergen_word_dict[allergen]
+        candidate_words = {word: counter[word] for word in counter if word not in words_accounted_for}
+        unanimous_max = True
+        max_count = max([v for k,v in counter.items()])
+        max_words = []
+        for word in counter:
+            if word not in words_accounted_for:
+                if counter[word] == max_count:
+                    if max_words:
+                        unanimous_max = False
+                    max_words.append(word)
+        if max_count == allergen_count[allergen]:
+            if unanimous_max and max_words:
+                final_allergen_word_map[allergen] = max_words[0]
+                words_accounted_for.add(max_words[0])
+            else:
+                for x in max_words:
+                    sus_words.add(x)
+print(','.join([final_allergen_word_map[k] for k in sorted(final_allergen_word_map)]))
