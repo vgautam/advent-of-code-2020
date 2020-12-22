f6a44eb927750e3e3904dbed07df5aca883aad05
vgautam <vasundhara1997@gmail.com>
2020-12-22 02:26:59 -0800

    day 22 part 2
    off-by-1 error that plagued me till i took a nap lol

--- day22.py
+++ day22.py
@@ -4,10 +4,6 @@ day=22 # update me
 inp_f = f'2020day{day:02d}input'
 #inp_f = 'test22'
 
-#lines = open(inp_f).read().split() # every word
-#lines = open(inp_f).readlines() # every line + \n
-#lines = [l.strip() for l in open(inp_f)] # every line
-#lines = [l.strip() for l in open(inp_f).read().split('\n\n')] # every block separated by 2 newlines
 lines = [l.split()[2:] for l in open(inp_f).read().split('\n\n')] # every block separated by 2 newlines, split into words
 
 player1 = [int(x) for x in lines[0]]
@@ -27,3 +23,34 @@ winner.reverse()
 print(sum([x*(i+1) for i,x in enumerate(winner)]))
 
 print('part 2')
+# pretty slow but watcha gonna do
+player1 = [int(x) for x in lines[0]]
+player2 = [int(x) for x in lines[1]]
+def recursive_combat(player1, player2):
+    previously_seen_configurations = []
+    while len(player1) != 0 and len(player2) != 0:
+        configuration = ';'.join([','.join([str(x) for x in player1]), ','.join([str(x) for x in player2])])
+        if configuration in previously_seen_configurations:
+            winner = (1, player1)
+            return winner
+        previously_seen_configurations.append(configuration)
+        play1 = player1.pop(0)
+        play2 = player2.pop(0)
+        if len(player1) >= play1 and len(player2) >= play2:
+            winner = recursive_combat(player1[:play1], player2[:play2])
+        else:
+            if play1 > play2:
+                winner = (1, player1)
+            elif play2 > play1:
+                winner = (2, player2)
+        if winner[0] == 1:
+            player1 += [play1, play2]
+            winner = (1, player1)
+        else:
+            player2 += [play2, play1]
+            winner = (2, player2)
+    return winner
+
+winner = recursive_combat(player1, player2)[1]
+winner.reverse()
+print(sum([x*(i+1) for i,x in enumerate(winner)]))
