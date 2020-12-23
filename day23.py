1b432c429eb384ec8c912bd24a1290472e0b1d82
vgautam <vasundhara1997@gmail.com>
2020-12-23 00:26:57 -0800

    day 23 part 2
    threw out my original solution and made a circular linked list but
    goddddd this was a struggle. I somehow thought I was WAY more
    comfortable with linked lists and generators and other things I used
    here than I actually turned out to be. Or I'm just having an off day :P

--- day23.py
+++ day23.py
@@ -1,49 +1,90 @@
 #!/usr/bin/env python3
 
+# is all this code ugly? yes
+# can i do better? also yes
+
 day=23 # update me
 inp_f = f'2020day{day:02d}input'
 
 lines = [int(c) for c in open(inp_f).read().strip()]
-lines = [int(c) for c in '389125467']
+#lines = [int(c) for c in '389125467']
 
-_min = min(lines)
-l = len(lines)
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+    def __str__(self):
+        return str(self.val)
+
+def initialize(lines):
+    node_mapping = [None] * (len(lines)+1)
+    first_node = Node(lines[0])
+    node_mapping[lines[0]] = first_node
+    prev_node = first_node
+    for c in lines[1:]:
+        new_node = Node(c)
+        node_mapping[c] = new_node
+        prev_node.next = new_node
+        prev_node = new_node
+    prev_node.next = first_node
+    return node_mapping, first_node
+
+# gotta love generators for cool shit like this <3
+# also i realized i am not as comfortable with generators as i probably should be
+def traverse(curr_node):
+    while True:
+        yield curr_node
+        curr_node = curr_node.next
 
 print('part 1')
-def crab_game(lines, num_moves):
+def crab_game(num_moves, _min, _max, first_node):
     i = 0
+    t = traverse(first_node)
     for iteration in range(num_moves):
-        cup = lines[i]
-        should_be_next = lines[(i+4)%l]
+        cup = next(t)
+        cup_val = cup.val
+        replace = cup.val - 1
         displaced_cups = []
+        displaced_cup_vals = []
         for _ in range(3):
-            d_c = lines.pop((lines.index(cup)+1)%len(lines))
+            d_c = next(t)
             displaced_cups.append(d_c)
-        _sorted = sorted(lines)
-        destination = _sorted[_sorted.index(cup)-1]
-        replace_after = lines.index(destination)
-        displaced_cups.reverse()
-        for d_c in displaced_cups:
-            lines.insert((replace_after+1)%l, d_c)
-        i = (i+1)%l
-        while not lines[i] == should_be_next:
-            lines = lines[1:] + [lines[0]]
-        assert lines[i] == should_be_next
-    return lines
+            displaced_cup_vals.append(d_c.val)
+        if replace < _min:
+            replace = _max
+        while replace in displaced_cup_vals:
+            replace -= 1
+            if replace < _min:
+                replace = _max
+        assert replace >= _min
+        replace_node = node_mapping[replace]
+        # update pointers
+        cup.next = displaced_cups[-1].next
+        t = traverse(curr_node=cup.next)
+        displaced_cups[-1].next = replace_node.next
+        replace_node.next = displaced_cups[0]
+        assert replace == replace_node.val
 
-lines = crab_game(lines, 100)
-while not lines[0] == 1:
-    lines = lines[1:] + [lines[0]]
-print(''.join([str(c) for c in lines[1:]]))
+_min = min(lines)
+_max = max(lines)
+node_mapping, first_node = initialize(lines)
+crab_game(100, _min, _max, first_node)
+
+answer = ''
+node = node_mapping[1].next
+t = traverse(node.next)
+while node.val != 1:
+    answer += str(node)
+    node = next(t)
+print(answer)
 
 print('part 2')
-# it's pretty clear this isn't going to work
-# the q is: do i now implement my own linked list from scratch or do i use a python builtin?
-# (also does one exist)
-_max = max(lines)
 for i in range(_max+1,1000001):
     lines.append(i)
-lines = crab_game(lines, 10000000)
-i = lines.index(1)
-print(lines[(i+1) % len(lines)])
-print(lines[(i+1) % len(lines)])
+_min = min(lines)
+_max = max(lines)
+node_mapping, first_node = initialize(lines)
+crab_game(10000000, _min, _max, first_node)
+node = node_mapping[1].next
+t = traverse(node)
+print(next(t).val * next(t).val)
